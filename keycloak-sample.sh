9e7228ac9045bd0d163b1684ad813a24e01316a3
Imran Suhail Ashraf <imran.suhail@10pearls.com>
2016-12-27 14:29:49 -0500

    this is the file

---
+++ keycloak-sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export TKN=$(curl -X POST 'http://localhost:8080/auth/realms/master/protocol/openid-connect/token' \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=admin" \
+ -d 'password=admin' \
+ -d 'grant_type=password' \
+ -d 'client_id=admin-cli' | jq -r '.access_token')
+
+curl -X GET 'http://localhost:8080/auth/admin/realms' \
+-H "Accept: application/json" \
+-H "Authorization: Bearer $TKN" | jq .
